7237d289da2b0736790d6568e105c04897768d6b
Kristin Beltz <beltz08@me.com>
2015-07-13 12:08:58 -0500

    Write link model.

---
+++ models/link.rb
@@ -0,0 +1,38 @@
+require_relative "../database_class_methods.rb"
+require_relative "../database_instance_methods.rb"
+
+class Link
+  extend DatabaseClassMethods
+  include DatabaseInstanceMethods
+
+
+  attr_accessor :url
+  attr_reader :id
+
+  # Initializes a new Assignment object
+  #
+  # id  - Integer id primary key created in the 'assignments' table
+  # url - String containing the name of the assignment
+  #
+  # Returns an Link object
+
+  def initialize(options = {})
+
+    # Example: {"id" => 1, "url" => "www.github.com"}
+    @id = options["id"]
+    @url = options["url"]
+
+  end
+
+  # method to validate link profiles before they are added to the database
+  #
+  # url - String containing a url
+  #
+  # Returns boolean
+  def self.valid?(url)
+    if url.empty?
+      true
+    end
+  end
+
+end
