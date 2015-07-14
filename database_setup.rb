5803e65f4c3bf012c4464fbb585f670d74a9ac5a
Kristin Beltz <beltz08@me.com>
2015-07-13 21:08:21 -0500

    Refactor tables.

--- database_setup.rb
+++ database_setup.rb
@@ -5,14 +5,9 @@ DATABASE.execute("PRAGMA foreign_keys = ON;")
 # Make the tables.
 DATABASE.execute("CREATE TABLE IF NOT EXISTS assignments (id INTEGER PRIMARY KEY, name TEXT NOT NULL, description TEXT NOT NULL);")
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY, url TEXT);")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY, url TEXT, assignment_id INTEGER, FOREIGN KEY (assignment_id) REFERENCES assignments(id));")
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS collaborators (id INTEGER PRIMARY KEY, name TEXT);")
-
-# Bridge tables
-DATABASE.execute("CREATE TABLE IF NOT EXISTS assignments_links (assignment_id INTEGER, link_id INTEGER, FOREIGN KEY (assignment_id) REFERENCES assignments(id), FOREIGN KEY (link_id) REFERENCES links(id));")
-
-DATABASE.execute("CREATE TABLE IF NOT EXISTS assignments_collaborators (assignment_id INTEGER, collaborator_id INTEGER, FOREIGN KEY (assignment_id) REFERENCES assignments(id), FOREIGN KEY (collaborator_id) REFERENCES collaborators(id));")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS collaborators (id INTEGER PRIMARY KEY, name TEXT, assignment_id INTEGER, FOREIGN KEY (assignment_id) REFERENCES assignments(id));")
 
 # Get results as an Array of Hashes.
 DATABASE.results_as_hash = true
